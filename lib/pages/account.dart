a11d3907d032a288ba2cd08772ff33e30682ae48
renatakhabirva <renatahabirovka49@gmail.com>
2025-05-11 07:23:45 +0500

    deploy

---
+++ lib/pages/account.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:gifthub/pages/profilepage.dart';
+import 'package:gifthub/pages/orders.dart';
+import 'package:gifthub/pages/notifications.dart';
+
+class AccountPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.account_circle),
+          onPressed: null,
+        ),
+        title: const Text('Аккаунт'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+
+            ListTile(
+              leading: Icon(Icons.account_circle_outlined),
+              title: Text('Профиль'),
+              onTap: () {
+                Navigator.pushNamed(
+                  context,
+                  '/profile',
+
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.shopping_bag_outlined),
+              title: Text('Заказы'),
+              onTap: () {
+                Navigator.pushNamed(
+                  context,
+                  '/orders',
+
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.notifications_none),
+              title: Text('Уведомления'),
+              onTap: () {
+                Navigator.pushNamed(
+                  context,
+                  '/notifications',
+
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
