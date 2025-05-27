b8ee28ab117648d0baee90df3d7017a3a13e76ae
renatakhabirova <khabirovarenataramisovna@gmail.com>
2025-05-27 19:05:21 +0500

    Delete lib/env_config.dart

--- lib/env_config.dart
+++
@@ -1,4 +0,0 @@
-const String supabaseUrl = 'https://ivelkowygsgeutmxhdwd.supabase.co';
-const String supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2ZWxrb3d5Z3NnZXV0bXhoZHdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE5Mjk3MTQsImV4cCI6MjA0NzUwNTcxNH0.uhTMOy3LMlMRne5HUcGWgV1QKW7_goPrLSDMpLLLVv4';
-const String yookassaShopId = '1088915';
-const String yookassaSecretKey = 'test_tph1vkcDxqmKges2aTeyq5wMoyKWtF3-WlnI4fwPMGA';
